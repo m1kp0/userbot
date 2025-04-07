23a422296bd70950e937e7a1b10a4fefaf38090d
m1kpe0 <yarikasik@inbox.ru>
2025-04-07 04:49:34 +0300

    Add files via upload

---
+++ Source/START.py
@@ -0,0 +1,5 @@
+from cmd_interface import print_start, select_settings
+
+if __name__ == "__main__":
+    print_start()
+    select_settings()
---
+++ Source/run_bot.py
@@ -0,0 +1,5 @@
+from client import client as cl
+
+def start_bot():
+    cl.m1kp.start()
+    cl.m1kp.run_until_disconnected()
