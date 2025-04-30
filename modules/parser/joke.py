8554f59c0dcae2066b730a34e2ba501ee4596b0e
m1kpe0 <yarikasik@inbox.ru>
2025-04-30 19:34:50 +0300

    Create joke.py

---
+++ modules/parser/joke.py
@@ -0,0 +1,22 @@
+from bs4 import BeautifulSoup
+import requests
+import random
+
+urls = [
+    "https://www.anekdot.ru/tags/пошлые/5?type=anekdots",
+    "https://anekdotov.net/intim/",
+    "https://anekdotov.net/anekdot/",
+    "https://anekdoty.ru/poshlye-anekdoty/10/",
+    "https://www.anekdot.ru/release/anekdot/day/2025-03-31/",
+    "https://www.maximonline.ru/entertainment/obyavleny-samye-smeshnye-anekdoty-2024-goda-id5823939/",
+    "https://anekdotov.net/anekdot/jew/"
+]
+
+
+def parse():
+    req = requests.get(urls[random.randint(0, 6)])
+    soup = BeautifulSoup(req.text, "html.parser")
+    jokes = soup.find_all("div", class_="text") or soup.find_all(
+        "div", class_="anekdot") or soup.find_all("div", class_="holder-body") or soup.find_all(
+            "div", class_="ds-block-text text-style-body-1 ds-article-content__block ds-article-content__block_text")
+    return [i.text for i in jokes]
