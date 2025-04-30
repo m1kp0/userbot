4cf9812cd736c7352e9b40a865395a1d88f586f8
m1kpe0 <yarikasik@inbox.ru>
2025-04-30 23:52:50 +0300

    Update joke.py

--- modules/parser/joke.py
+++ modules/parser/joke.py
@@ -3,18 +3,34 @@ import requests
 import random
 
 urls = [
+    "https://www.anekdot.ru/last/good/",
+    "https://www.anekdot.ru/last/anekdot/",
+    "https://www.anekdot.ru/random/anekdot/",
+    "https://www.anekdot.ru/release/anekdot/week/",
+    "https://www.anekdot.ru/release/anekdot/month/",
+    "https://www.anekdot.ru/release/anekdot/year/",
     "https://www.anekdot.ru/tags/пошлые/5?type=anekdots",
+    "https://www.anekdot.ru/release/anekdot/day/2025-03-31/",
+
+    "https://anekdoty.ru/",
+    "https://anekdoty.ru/korotkie/",
+    "https://anekdoty.ru/pro-negrov/",
+    "https://anekdoty.ru/pro-gitlera/",
+    "https://anekdoty.ru/cherniy-yumor/",
+    "https://anekdoty.ru/samye-smeshnye/",
+    "https://anekdoty.ru/tupo-no-smeshno/",
+    "https://anekdoty.ru/poshlye-anekdoty/10/",
+
     "https://anekdotov.net/intim/",
     "https://anekdotov.net/anekdot/",
-    "https://anekdoty.ru/poshlye-anekdoty/10/",
-    "https://www.anekdot.ru/release/anekdot/day/2025-03-31/",
-    "https://www.maximonline.ru/entertainment/obyavleny-samye-smeshnye-anekdoty-2024-goda-id5823939/",
-    "https://anekdotov.net/anekdot/jew/"
+    "https://anekdotov.net/anekdot/jew/",
+
+    "https://www.maximonline.ru/entertainment/obyavleny-samye-smeshnye-anekdoty-2024-goda-id5823939/"
 ]
 
 
-def parse():
-    req = requests.get(urls[random.randint(0, 6)])
+def parse_joke():
+    req = requests.get(urls[random.randint(0, 19)])
     soup = BeautifulSoup(req.text, "html.parser")
     jokes = soup.find_all("div", class_="text") or soup.find_all(
         "div", class_="anekdot") or soup.find_all("div", class_="holder-body") or soup.find_all(
